78eaaec5641651c77cd43028c6f842b99fe9a4d5
Amar Imširović <imsirovicamar000@gmail.com>
2022-05-24 14:05:47 +0200

    you can now select recipes and add new ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,13 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
-    new Recipe('Test','This is a test', 'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_960_720.jpg')
+    new Recipe('Test','This is a test', 'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_960_720.jpg'),
+    new Recipe('Test2','This is a test2', 'https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_960_720.jpg'),
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+  } 
 
 }
